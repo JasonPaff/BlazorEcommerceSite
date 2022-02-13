bca558cc8cf94bc4e75460074e3ad32a09639329
Jason <jasonpaff@gmail.com>
2022-02-12 19:01:20 -0500

    client category service

--- Client/Services/ProductService/IProductService.cs
+++ Client/Services/ProductService/IProductService.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ECommerce.Client.Services.ProductService
 {
     public interface IProductService
     {
+        event Action ProductsChanged;
         List<Product> Products { get; set; }
-        Task GetProducts();
+        Task GetProducts(string? categoryUrl = null);
         Task<ServiceResponse<Product>?> GetProduct(int productId);
     }
 }
--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -12,18 +13,27 @@ namespace ECommerce.Client.Services.ProductService
         // inject http service
         public ProductService(HttpClient http)
         {
-            _http = http; 
+            _http = http;
         }
-        
-        // products list
+
+        // products list;
         public List<Product> Products { get; set; } = new();
+
+        // products changed action
+        public event Action ProductsChanged;
         
         // get all products from the products
-        public async Task GetProducts()
+        public async Task GetProducts(string? categoryUrl = null)
         {
-            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
-            if (result is {Data: { } }) 
+            // get all products or category specific products
+            var result = categoryUrl == null
+                ? await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product")
+                : await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
+            if (result is {Data: { }})
                 Products = result.Data;
+
+            // fire products changed event
+            ProductsChanged.Invoke();
         }
 
         // get a single product from the products controller on the server

fa98a9d5c71d59069b1502b2780dc07daa752f91
Jason <jasonpaff@gmail.com>
2022-02-13 14:22:23 -0500

    user model attributes

--- Shared/UserRegister.cs
+++ Shared/UserRegister.cs
@@ -1,11 +1,14 @@
-﻿using System.Security.AccessControl;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ECommerce.Shared
 {
     public class UserRegister
     {
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required, StringLength(100, MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
+        [Compare("Password", ErrorMessage = "The passwords do not match.")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }

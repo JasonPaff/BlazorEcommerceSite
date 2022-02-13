5a5d91a28245e86cdde873fbbbf51fda5e67df8b
Jason <jasonpaff@gmail.com>
2022-02-12 19:44:00 -0500

    product type/variant integration

--- Server/Services/ProductService/ProductService.cs
+++ Server/Services/ProductService/ProductService.cs
@@ -20,7 +20,9 @@ namespace ECommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products
+                    .Include(p => p.Variants) // include Variants
+                    .ToListAsync()
             };
 
             return response;
@@ -30,7 +32,10 @@ namespace ECommerce.Server.Services.ProductService
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)// include Variants
+                .ThenInclude(v => v.ProductType) // include ProductTypes
+                .FirstOrDefaultAsync(p => p.Id == productId);
 
             if (product is null)
             {
@@ -49,7 +54,8 @@ namespace ECommerce.Server.Services.ProductService
             var response = new ServiceResponse<List<Product>>
             {
                 Data = await _context.Products
-                    .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                    .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))// matching category
+                    .Include(p => p.Variants) // include Variants
                     .ToListAsync()
             };
 
